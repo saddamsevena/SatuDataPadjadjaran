bb3f964c640508870e628264f17af024af8f4ff4
saddamsevena <saddamsevena48@gmail.com>
2023-01-18 16:24:24 +0700

    Fix url

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -73,9 +73,9 @@ class HomeController extends Controller
 
     public function editProfile($id)
     {
-        $user = User::findOrFail($id);
+        $user = User::findOrFail(Auth::user()->npm);
         $datas = Data::where('user_npm', Auth::user()->id)->get();
-        return view("profile", compact("user"), ['datas'=>$datas->sortByDesc('updated_at')]);
+        return view('profile', compact('user'), ['datas'=>$datas->sortByDesc('updated_at')]);
     }
 
     public function updateProfile(Request $request) 
