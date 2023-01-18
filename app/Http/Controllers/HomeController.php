f0b9e6c9f0c9cc8c8ed93f9e8d8417b8ca4fff6a
saddamsevena <saddamsevena48@gmail.com>
2023-01-18 21:20:46 +0700

    Profile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,7 +74,7 @@ class HomeController extends Controller
     public function editProfile($id)
     {
         $user = User::findorfail($id);
-        $datas = Data::where('user_npm', Auth::user()->id)->get();
+        $datas = Data::where('user_npm', Auth::user()->npm)->get();
         return view('profile', compact('user'), ['datas'=>$datas->sortByDesc('updated_at')]);
     }
 
